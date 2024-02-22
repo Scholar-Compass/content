4bc82cad17d3d1ecf80abc994f0d38f166141bb2
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2024-02-22 15:40:38 +0800

    feat(macro): 胡话

--- hooks/macros.py
+++ hooks/macros.py
@@ -57,3 +57,31 @@ def define_env(env: MacrosPlugin):
     <iframe loading="lazy" src="https://player.bilibili.com/player.html?autoplay=0&as_wide=1&bvid={bvid}"></iframe>
 </aside>
 """
+
+    @env.macro
+    def just_kidding(text: str, title: str = "只为博君一笑，不必照单全收XD") -> str:
+        """胡话
+
+        一段被划掉的文字，适合标注玩笑话、胡话，或者虽然真实但完全只因幽默效果而被提及的内容。
+
+        请勿滥用。（为此还特意采用了比 markdown（`~~`）更复杂的语法）
+
+        请尽量保持语句连贯——无论是否真的划掉文字，最好语句都通顺，且标点符号也正确。
+
+        # 示例
+
+        ```
+        中国人民大学简称“人大”{{ just_kidding("（不是“人民代表大会”）") }}，是中国共产党创办的第一所新型正规大学。
+        ```
+
+        # 参数
+
+        - `text`：要划掉的文字。
+        - `title`：鼠标放到文字上显示的内容，默认为“只为博君一笑，不必照单全收XD”。
+
+        # 相关链接
+
+        [模板:`胡话` - 萌娘百科 万物皆可萌的百科全书](https://zh.moegirl.org.cn/Template:胡话)
+        """
+        # 依赖`extra.css`
+        return f'<s class="just-kidding" title="{title}">{text}</s>'
